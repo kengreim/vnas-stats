2032c4044111ac7a59a78751bf1dffd0f5edf599
Ken Greim <34892440+kengreim@users.noreply.github.com>
2025-11-27 10:32:55 -0800

    Initial addition of healthcheck for datafeed_fetcher

--- datafeed_fetcher/src/main.rs
+++ datafeed_fetcher/src/main.rs
@@ -2,7 +2,13 @@
 mod error;
 
 use crate::error::{EnqueueError, FetchError};
-use chrono::{DateTime, Utc};
+use axum::Router;
+use axum::extract::State;
+use axum::http::StatusCode;
+use axum::response::IntoResponse;
+use axum::routing::get;
+use chrono::{DateTime, TimeDelta, Utc};
+use parking_lot::RwLock;
 use serde_json::Value;
 use shared::PostgresConfig;
 use shared::error::InitializationError;
@@ -10,7 +16,9 @@ use shared::load_config;
 use shared::vnas::datafeed::{VnasEnvironment, datafeed_url};
 use sqlx::postgres::PgPoolOptions;
 use sqlx::{Pool, Postgres};
+use std::sync::Arc;
 use std::time::Duration;
+use tokio::net::TcpListener;
 use tokio::time::sleep;
 use tracing::{debug, error, info, warn};
 use tracing_subscriber::EnvFilter;
@@ -29,17 +37,54 @@ async fn main() -> Result<(), InitializationError> {
 
     // Set up config
     let config = load_config().unwrap_or_else(|e| {
-        error!(error = ?e, "Configuration could not be initialized");
-        panic!("Configuration could not be initialized");
+        error!(error = ?e, "configuration could not be initialized");
+        panic!("configuration could not be initialized");
     });
 
     let db_pool = initialize_db(&config.postgres).await?;
 
+    let last_attempted_update = Arc::new(RwLock::new(None));
+    let last_successful_update = Arc::new(RwLock::new(None));
+    let last_error = Arc::new(RwLock::new(None));
+
+    let last_attempted_update_clone = Arc::clone(&last_attempted_update);
+    let last_successful_update_clone = Arc::clone(&last_successful_update);
+    let last_error_clone = Arc::clone(&last_error);
+
+    let axum_handle = tokio::spawn(async move {
+        run_health_server(
+            last_attempted_update_clone,
+            last_successful_update_clone,
+            last_error_clone,
+        )
+        .await
+    });
+
+    let fetcher_handle = tokio::spawn(async move {
+        fetcher_loop(
+            db_pool,
+            last_attempted_update,
+            last_successful_update,
+            last_error,
+        )
+        .await
+    });
+
+    let _ = tokio::join!(axum_handle, fetcher_handle);
+
+    Ok(())
+}
+
+async fn fetcher_loop(
+    db_pool: Pool<Postgres>,
+    last_attempted_update: Arc<RwLock<Option<DateTime<Utc>>>>,
+    last_successful_update: Arc<RwLock<Option<DateTime<Utc>>>>,
+    last_error: Arc<RwLock<Option<EnqueueError>>>,
+) {
     // Default reqwest client
     let http_client = reqwest::Client::new();
 
-    // Datafetcher infinite loop
-    info!("Initialized Datafeed Fetcher");
+    info!("initialized Datafeed Fetcher");
     let mut initial_loop = true;
     let mut previous_timestamp: Option<DateTime<Utc>> = None;
     loop {
@@ -49,10 +94,13 @@ async fn main() -> Result<(), InitializationError> {
             sleep(Duration::from_secs(15)).await;
         }
 
+        let now = Utc::now();
+        *last_attempted_update.write() = Some(now);
         let (payload, current_timestamp) = match fetch_datafeed(&http_client).await {
             Ok((p, t)) => (p, t),
             Err(e) => {
-                warn!(error = ?e, "Failed to fetch and deserialize datafeed");
+                warn!(error = ?e, "failed to fetch and deserialize datafeed");
+                *last_error.write() = Some(e.into());
                 continue;
             }
         };
@@ -63,20 +111,90 @@ async fn main() -> Result<(), InitializationError> {
         {
             info!(
                 timestamp = ?previous_timestamp,
-                "Found no change to datafeed"
+                "found no change to datafeed"
             );
+            *last_successful_update.write() = Some(now);
             continue;
         }
 
-        info!(timestamp = ?current_timestamp, "Found updated datafeed");
+        info!(timestamp = ?current_timestamp, "found updated datafeed");
         previous_timestamp = Some(current_timestamp);
 
         if let Err(e) = enqueue_datafeed(&db_pool, payload, current_timestamp).await {
-            warn!(error = ?e, "Could not enqueue datafeed into Postgres");
+            warn!(error = ?e, "could not enqueue datafeed into Postgres");
+            *last_error.write() = Some(e);
             continue;
         } else {
-            debug!("Enqueued datafeed into Postgres queue");
+            *last_successful_update.write() = Some(now);
+            debug!("enqueued datafeed into Postgres queue");
         }
+    }
+}
+
+#[derive(Clone)]
+struct AxumState {
+    last_attempted_update: Arc<RwLock<Option<DateTime<Utc>>>>,
+    last_successful_update: Arc<RwLock<Option<DateTime<Utc>>>>,
+    last_error: Arc<RwLock<Option<EnqueueError>>>,
+}
+
+async fn run_health_server(
+    last_attempted_update: Arc<RwLock<Option<DateTime<Utc>>>>,
+    last_successful_update: Arc<RwLock<Option<DateTime<Utc>>>>,
+    last_error: Arc<RwLock<Option<EnqueueError>>>,
+) {
+    info!("starting axum health server");
+    let app = Router::new()
+        .route("/health", get(health_check))
+        .with_state(AxumState {
+            last_successful_update,
+            last_attempted_update,
+            last_error,
+        });
+    let listener = TcpListener::bind("127.0.0.1:3000").await.unwrap();
+    axum::serve(listener, app).await.unwrap();
+}
+
+async fn health_check(State(state): State<AxumState>) -> impl IntoResponse {
+    let last_attempted_update = *state.last_attempted_update.read();
+    let last_successful_update = *state.last_successful_update.read();
+    let last_error = if let Some(e) = state.last_error.read().as_ref() {
+        format!("{e:?}")
+    } else {
+        "unknown".to_string()
+    };
+
+    if last_attempted_update.is_none() || last_successful_update.is_none() {
+        if let Some(last_attempted_update) = last_attempted_update {
+            return (
+                StatusCode::INTERNAL_SERVER_ERROR,
+                format!(
+                    "Datafeed has not been successfully updated. Last attempted update: {last_attempted_update}. Last error: {last_error}"
+                ),
+            );
+        } else {
+            return (
+                StatusCode::INTERNAL_SERVER_ERROR,
+                "No attempted or successful datafeed updates".to_string(),
+            );
+        }
+    }
+
+    // We can safely unwrap here because we checked is_none above
+    let last_attempted_update = last_attempted_update.unwrap();
+    let last_successful_update = last_successful_update.unwrap();
+    if (Utc::now() - last_successful_update) > TimeDelta::seconds(60) {
+        (
+            StatusCode::INTERNAL_SERVER_ERROR,
+            format!(
+                "Datafeed not fetched in the last 60 seconds. Last successful update: {last_successful_update}. Last attempted updated: {last_attempted_update}. Last error: {last_error}"
+            ),
+        )
+    } else {
+        (
+            StatusCode::OK,
+            format!("Datafeed last successfully fetched: {last_successful_update}"),
+        )
     }
 }
 
