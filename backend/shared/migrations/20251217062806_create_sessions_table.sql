7a8230c90eb60f9b16f681cfcff46ed13e219d13
Ken Greim <34892440+kengreim@users.noreply.github.com>
2025-12-17 09:53:56 -0800

    Modify sessions migration to create new schema

--- backend/shared/migrations/20251217062806_create_sessions_table.sql
+++ backend/shared/migrations/20251217062806_create_sessions_table.sql
@@ -1,5 +1,7 @@
-CREATE TABLE IF NOT EXISTS tower_sessions (
+CREATE SCHEMA IF NOT EXISTS data_api;
+
+CREATE TABLE IF NOT EXISTS data_api.sessions (
     id text PRIMARY KEY NOT NULL,
-    expiry_date timestamptz NOT NULL,
     data bytea NOT NULL
+    expiry_date timestamptz NOT NULL,
 );
