02dd7f25cc6ddbd9b76b1d56fca353d8171581b5
Ken Greim <34892440+kengreim@users.noreply.github.com>
2025-12-17 12:07:41 -0800

    Fix sessions sql migration

--- backend/shared/migrations/20251217062806_create_sessions_table.sql
+++ backend/shared/migrations/20251217062806_create_sessions_table.sql
@@ -2,6 +2,6 @@ CREATE SCHEMA IF NOT EXISTS data_api;
 
 CREATE TABLE IF NOT EXISTS data_api.sessions (
     id text PRIMARY KEY NOT NULL,
-    data bytea NOT NULL
-    expiry_date timestamptz NOT NULL,
+    data bytea NOT NULL,
+    expiry_date timestamptz NOT NULL
 );
