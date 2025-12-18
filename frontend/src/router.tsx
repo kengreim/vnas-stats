e9b67f9e7338c8b2eac9cb053079cfe14565af20
Ken Greim <34892440+kengreim@users.noreply.github.com>
2025-12-18 13:22:53 -0800

    Add redirect from home page to privacy page for now

--- frontend/src/router.tsx
+++ frontend/src/router.tsx
@@ -1,5 +1,11 @@
-import { createRouter, Outlet, createRootRoute, createRoute } from "@tanstack/solid-router";
-import App from "./App";
+import {
+  createRouter,
+  Outlet,
+  createRootRoute,
+  createRoute,
+  Navigate,
+  redirect,
+} from "@tanstack/solid-router";
 import { Privacy } from "~/components/Privacy.tsx";
 
 const rootRoute = createRootRoute({
@@ -12,13 +18,21 @@ const rootRoute = createRootRoute({
 //   component: App,
 // });
 
+const indexRoute = createRoute({
+  getParentRoute: () => rootRoute,
+  path: "/",
+  loader: () => {
+    throw redirect({ to: "/privacy" });
+  },
+});
+
 const privacyRoute = createRoute({
   getParentRoute: () => rootRoute,
   path: "/privacy",
   component: Privacy,
 });
 
-const routeTree = rootRoute.addChildren([privacyRoute]);
+const routeTree = rootRoute.addChildren([indexRoute, privacyRoute]);
 
 export const router = createRouter({ routeTree });
 
